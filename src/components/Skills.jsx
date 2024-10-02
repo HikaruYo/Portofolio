ac0292bbb79c39d759e9aea736ac69a81906d290
HikaruYo <adithaean@gmail.com>
2024-10-02 16:02:04 +0700

    Update Skills Page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,28 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import ScrollReveal from 'scrollreveal';
 
-const FrontEnd = () => {
+const Language = () => {
     return (
-        <div className="skills-box">
-            <i className='bx bx-code'></i>
-            <h3>FrontEnd</h3>
-            <p>HTML, CSS, JavaScript</p>
+        <div className="skills-box-section">
+            <div className="skills-box">
+                <i className="devicon-html5-plain"></i>
+                <h3>HTML</h3>
+            </div>
+            <div className="skills-box">
+                <i className="devicon-css3-plain"></i>
+                <h3>CSS</h3>
+            </div>
+            <div className="skills-box">
+                <i className="devicon-php-plain"></i>
+                <h3>PHP</h3>
+            </div>
+            <div className="skills-box">
+                <i className="devicon-python-plain"></i>
+                <h3>Python</h3>
+            </div>
+            <div className="skills-box">
+                <i className="devicon-javascript-plain"></i>
+                <h3>Javascript</h3>
+            </div>
         </div>
     )
 }
 
-const BackEnd = () => {
+const Framework = () => {
     return (
-        <div className="skills-box">
-            <i className='bx bx-minus-back'></i>
-            <h3>BackEnd</h3>
-            <p>Python, PHP, MySql</p>
+        <div className="skills-box-section">
+            <div className="skills-box">
+                <i className="devicon-laravel-original"></i>
+                <h3>Laravel</h3>
+            </div>
+            <div className="skills-box">
+                <i className="devicon-react-original"></i>
+                <h3>React</h3>
+            </div>
         </div>
     )
 }
 
 function Skills() {
-    const [activeComponent, setActiveComponent] = useState('FrontEnd'); // State untuk melacak komponen aktif
+    const [activeComponent, setActiveComponent] = useState('Language');
 
     useEffect(() => {
         // ScrollReveal
@@ -33,10 +55,15 @@ function Skills() {
         });
 
         ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-        ScrollReveal().reveal('.home-img, .skills-container, .project-box, .contact form', { origin: 'bottom' });
+        ScrollReveal().reveal('.home-img, .skills-container, .project-box', { origin: 'bottom' });
         ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
         ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
     }, []);
+
+    // UseEffect kedua untuk memanggil ScrollReveal setiap kali activeComponent berubah
+    useEffect(() => {
+        ScrollReveal().reveal('.skills-box-section .skills-box', { origin: 'bottom' });
+    }, [activeComponent]);
 
     return (
         <section className="skills" id="skills">
@@ -44,13 +71,25 @@ function Skills() {
 
             <div className="skills-container">
                 <div className="skills-header">
-                    <a href="#!" onClick={() => setActiveComponent('FrontEnd')}>Front End</a>
-                    <a href="#!" onClick={() => setActiveComponent('BackEnd')}>Back End</a>
+                    <a
+                        href="#!"
+                        onClick={() => setActiveComponent('Language')}
+                        className={activeComponent === 'Language' ? 'active' : ''}
+                    >
+                        Language
+                    </a>
+                    <a
+                        href="#!"
+                        onClick={() => setActiveComponent('Framework')}
+                        className={activeComponent === 'Framework' ? 'active' : ''}
+                    >
+                        Framework/Library
+                    </a>
                 </div>
 
                 {/* Render komponen berdasarkan state aktif */}
-                {activeComponent === 'FrontEnd' && <FrontEnd />}
-                {activeComponent === 'BackEnd' && <BackEnd />}
+                {activeComponent === 'Language' && <Language />}
+                {activeComponent === 'Framework' && <Framework />}
             </div>
         </section>
     );

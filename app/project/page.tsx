ec9c5a9515c10172c4e14b3bebf999354192eec8
HikaruYo <adithaean@gmail.com>
2025-09-04 22:53:45 +0700

    Fixing Mobile View for Project Card

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -15,23 +15,32 @@ export default function AboutPage() {
 
       {/* Main Content */}
       <div className="grid w-full place-content-center items-center">
+        <motion.div
+          initial={{opacity: 0, y: -50}}
+          animate={{opacity: 1, y: 0}}
+          transition={{duration: 1}}
+          className="mb-2 md:mb-6"
+        >
+          <h2 className="text-4xl text-center font-bold">My <span
+            className="text-gray-600 dark:text-gray-400 transition duration-500">Project</span></h2>
+        </motion.div>
 
         {/* Card 1 */}
         <motion.div
           initial={{opacity: 0, y: 50}}
           animate={{opacity: 1, y: 0}}
           transition={{duration: 1}}
-          className="bg-[#495057] flex p-7 space-x-12 rounded-xl max-w-xl mx-auto"
+          className="bg-[#495057] flex flex-col md:flex-row p-2 md:p-7 md:space-x-10 rounded-xl md:max-w-xl mx-auto items-center justify-center"
         >
           {/* Image */}
           <Image
             src={Cuan}
             alt="Cuan Project"
-            className="w-44 h-56 object-cover"
+            className="w-44 h-64 object-cover"
           >
           </Image>
           {/* Text */}
-          <div className="space-y-4 text-white">
+          <div className="space-y-4 text-white text-left p-6 md:p-0">
             <h3 className="text-3xl font-bold">Cuan: Catatan Keuangan</h3>
             <p>{ desk }</p>
             <div className="flex items-center justify-center w-full">
